a5c84b170ba37899cc14f1c30f7ad387755d31f9
6426mohaN <getiyenahom751@gmail.com>
2024-06-14 11:38:21 +0300

    finished the structure for the front-end

---
+++ src/app/(root)/layout.tsx
@@ -0,0 +1,12 @@
+import React, { Children, ReactNode } from 'react'
+
+const RootLayout = ({children}:{children:ReactNode}) => {
+  return (
+<main>
+    {children}
+    
+</main>
+  )
+}
+
+export default RootLayout
---
+++ src/app/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Defence engineering college",
+  description: "created by Nahom Getiye",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+        <body className={`${inter.className}  bg-dark-2`}>{children}</body>
+
+    </html>
+  );
+}
